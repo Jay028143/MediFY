88f00be7f7f4ad9e07e2bde62c7a40c57e390942
Jay Patel <p028143m@student.staffs.ac.uk>
2023-03-18 17:40:36 +0530

    MediFY Project Creation

---
+++ src/main/java/com/medify/MediFyApplication.java
@@ -0,0 +1,13 @@
+package com.medify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediFyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediFyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medify/MediFyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediFyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
